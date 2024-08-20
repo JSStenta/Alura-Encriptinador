1b5c6cdc3311ccd67d1a91ad3b97650173261c94
Juansezhin <jsstenta@outlook.com>
2024-08-20 16:50:43 -0300

    Version funcional, falta corregir el input

--- encriptainador.js
+++ encriptainador.js
@@ -1,6 +1,20 @@
+// Estado inicial de la página
 let texto = '';
 limparEncriptacion();
+document.getElementById('textoUsuario').onfocus = function () { this.value = (this.value == 'Ingrese el texto aquí') ? '' : this.value; }
+document.getElementById('textoUsuario').onblur = function () { this.value = (this.value == '') ? 'Ingrese el texto aquí' : this.value; }
+function limparEncriptacion() {
+    let elementosVacios = document.getElementsByClassName('resultado__vacio');
+    let elementosSalida = document.getElementsByClassName('resultado__salida');
+    for (let i = 0; i < elementosVacios.length; i++) {
+        elementosVacios[i].removeAttribute('hidden');
+    }
+    for (let i = 0; i < elementosSalida.length; i++) {
+        elementosSalida[i].setAttribute('hidden', true);
+    }
+}
 
+// Funcion generica para insertar un texto dado en determinado elemento
 function insertarTextoEnElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
@@ -8,6 +22,7 @@ function insertarTextoEnElemento(elemento, texto) {
 }
 
 /*
+    Funcion para encriptar el mensaje de enetrada
     La letra "a" es convertida para "ai"
     La letra "e" es convertida para "enter"
     La letra "i" es convertida para "imes"
@@ -16,61 +31,66 @@ function insertarTextoEnElemento(elemento, texto) {
 */
 function encriptainador() {
     let textoUsuario = document.getElementById('textoUsuario').value;
-    textoUsuario = textoUsuario.toLowerCase();
-    let salida = '';
-    for (caracter of textoUsuario) {
-        switch (caracter) {
-            case 'a':
-                salida += 'ai';
-                break;
-            case 'e':
-                salida += 'enter';
-                break;
-            case 'i':
-                salida += 'imes';
-                break;
-            case 'o':
-                salida += 'ober';
-                break;
-            case 'u':
-                salida += 'ufat';
-                break;
-            default:
-                salida += caracter;
-                break;
+    textoUsuario = textoUsuario;
+    if (textoUsuario !== 'Ingrese el texto aquí') {
+        let salida = '';
+        for (caracter of textoUsuario) {
+            switch (caracter.toLowerCase()) {
+                case 'a':
+                    salida += caracter + 'i';
+                    break;
+                case 'e':
+                    salida += caracter + 'nter';
+                    break;
+                case 'i':
+                    salida += caracter + 'mes';
+                    break;
+                case 'o':
+                    salida += caracter + 'ber';
+                    break;
+                case 'u':
+                    salida += caracter + 'fat';
+                    break;
+                default:
+                    salida += caracter;
+                    break;
+            }
         }
+        texto = salida;
+        mostrarEncriptacion(salida);
     }
-    texto = salida;
-    mostrarEncriptacion(salida);
     return;
 }
 
+// Funcion para desencriptar el mensaje de enetrada
 function desencriptainador() {
     let textoUsuario = document.getElementById('textoUsuario').value;
-    textoUsuario = textoUsuario.toLowerCase();
-    let salida = '';
-    for (let i = 0; i < textoUsuario.length; i++) {
-        let caracter = textoUsuario[i];
-        salida += caracter;
-        switch (caracter) {
-            case 'a':
-                i += 1;
-                break;
-            case 'e':
-                i += 4;
-                break;
-            case 'i':
-            case 'o':
-            case 'u':
-                i += 3;
-                break;
+    if (textoUsuario !== 'Ingrese el texto aquí') {
+        let salida = '';
+        for (let i = 0; i < textoUsuario.length; i++) {
+            let caracter = textoUsuario[i];
+            salida += caracter;
+            switch (caracter.toLowerCase()) {
+                case 'a':
+                    i += 1;
+                    break;
+                case 'e':
+                    i += 4;
+                    break;
+                case 'i':
+                case 'o':
+                case 'u':
+                    i += 3;
+                    break;
+            }
         }
+        texto = salida;
+        mostrarEncriptacion(salida);
     }
-    texto = salida;
-    mostrarEncriptacion(salida);
     return;
 }
 
+//Muestra el resultado de la encriptacion
 function mostrarEncriptacion(texto) {
     let elementosVacios = document.getElementsByClassName('resultado__vacio');
     let elementosSalida = document.getElementsByClassName('resultado__salida');
@@ -83,17 +103,7 @@ function mostrarEncriptacion(texto) {
     insertarTextoEnElemento('.resultado__salida__texto', texto);
 }
 
+// Funcion de copiar salida
 function copiarEncriptada() {
     navigator.clipboard.writeText(texto);
-}
-
-function limparEncriptacion() {
-    let elementosVacios = document.getElementsByClassName('resultado__vacio');
-    let elementosSalida = document.getElementsByClassName('resultado__salida');
-    for (let i = 0; i < elementosVacios.length; i++) {
-        elementosVacios[i].removeAttribute('hidden');
-    }
-    for (let i = 0; i < elementosSalida.length; i++) {
-        elementosSalida[i].setAttribute('hidden', true);
-    }
 }
